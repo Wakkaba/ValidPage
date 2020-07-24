f03990690ed53746b153ffb8018611bf0ed8b8fb
Arthur Zarytsky <skyzarfranko@gmail.com>
2020-07-24 15:09:12 +0300

    Updates

---
+++ precache-manifest.eaeac5cce05949a16000463034c81c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532e9850b882c0a10f163fac98b1e809",
+    "url": "/Wakkaba-github-io/index.html"
+  },
+  {
+    "revision": "7e031946df6eedad5040",
+    "url": "/Wakkaba-github-io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d047f072a0c5a76ac709",
+    "url": "/Wakkaba-github-io/static/js/2.c5004aa0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Wakkaba-github-io/static/js/2.c5004aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e031946df6eedad5040",
+    "url": "/Wakkaba-github-io/static/js/main.c3bbdfda.chunk.js"
+  },
+  {
+    "revision": "f34a5e00be1249d2ae2d",
+    "url": "/Wakkaba-github-io/static/js/runtime-main.f2014e51.js"
+  }
+]);
